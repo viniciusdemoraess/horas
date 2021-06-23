fc40ae06f1fbb6a4988ced0c809b2f9aeda2cd4e
viniciusdemoraess <demoraesvini@outlook.com>
2021-06-22 20:13:31 -0400

    Pequeno documento para testes js html e css

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 19 //Para testar 
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if(hora >=0 && hora <12){
+        //Bom Dia!
+
+        img.src = 'manha-menor.png'
+        document.body.style.background = '#ac961dce'
+
+    } else if (hora >=12 && hora <=18){
+        //Boa Tarde!
+
+        img.src = 'tarde-menor.png'
+        document.body.style.background = '#efb765'
+
+    } else{
+        //Boa Noite!
+
+        img.src = 'noite-menor.png'
+        document.body.style.background = '#162645'
+    }
+
+}
+    
